c126a57a69615b8b61351d8e822862fce43ac4a0
cluyeye <coxebernardo@outlook.com>
2021-03-31 11:37:13 +0100

    save changes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "fd2eac31c35a78b72df5";
+/******/ 	var hotCurrentHash = "d0cd452f99251af57188";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
